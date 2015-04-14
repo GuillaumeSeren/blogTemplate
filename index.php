74418cb64f2e96cd24f8f6766e2075866fab1588
Guillaume Seren <guillaumeseren@gmail.com>
2015-04-14 18:38:09 +0200

    Index add licence.

--- index.php
+++ index.php
@@ -1,17 +1,24 @@
-<?php defined('_JEXEC') or die('Restricted access');?>
+<?php
+/**
+ * @package     GuillaumeSeren.BlogTemplate
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// No direct access.
+defined('_JEXEC') or die('Restricted access');?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml"
     xml:lang="<?php echo $this->language; ?>"
     lang="<?php echo $this->language; ?>" >
-<head>
-    <jdoc:include type="head" />
-    <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/system.css" type="text/css" />
-    <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/general.css" type="text/css" />
-    <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/template.css" type="text/css" />
-</head>
-<body>
-    <jdoc:include type="modules" name="top" />
-    <jdoc:include type="component" />
-    <jdoc:include type="modules" name="bottom" />
-</body>
+    <head>
+        <jdoc:include type="head" />
+        <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/system.css" type="text/css" />
+        <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/general.css" type="text/css" />
+        <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/template.css" type="text/css" />
+    </head>
+    <body>
+        <jdoc:include type="modules" name="top" />
+        <jdoc:include type="component" />
+        <jdoc:include type="modules" name="bottom" />
+    </body>
 </html>
