1f5f7a413d0ba6f05fc250ca9af8f96729e5f734
Guillaume Seren <guillaumeseren@gmail.com>
2015-04-29 10:56:13 +0200

    Clean format template.

--- index.php
+++ index.php
@@ -12,9 +12,18 @@ defined('_JEXEC') or die('Restricted access');?>
     lang="<?php echo $this->language; ?>" >
     <head>
         <jdoc:include type="head" />
-        <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/system.css" type="text/css" />
-        <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/general.css" type="text/css" />
-        <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/template.css" type="text/css" />
+        <link
+            rel="stylesheet"
+            href="<?php echo $this->baseurl ?>/templates/system/css/system.css"
+            type="text/css" />
+        <link
+            rel="stylesheet"
+            href="<?php echo $this->baseurl ?>/templates/system/css/general.css"
+            type="text/css" />
+        <link
+            rel="stylesheet"
+            href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/template.css"
+            type="text/css" />
     </head>
     <body>
         <jdoc:include type="modules" name="top" />
