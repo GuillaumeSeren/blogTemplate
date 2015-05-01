613c594b35d9210343c1214edcd592fab2d90bec
Guillaume Seren <guillaumeseren@gmail.com>
2015-05-01 12:11:02 +0200

    Add normalize.css submodule.

--- index.php
+++ index.php
@@ -28,6 +28,10 @@ $app = JFactory::getApplication();
             type="text/css" />
         <link
             rel="stylesheet"
+            href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/bundle/normalize.css/normalize.css"
+            type="text/css" />
+        <link
+            rel="stylesheet"
             href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/template.css"
             type="text/css" />
     </head>
